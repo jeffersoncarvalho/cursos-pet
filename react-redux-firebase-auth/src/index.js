41920e4d8e2cec22663aed29bf6f3012f4da93aa
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2020-06-19 10:38:17 -0300

    persistindo o estado geral

--- react-redux-firebase-auth/src/index.js
+++ react-redux-firebase-auth/src/index.js
@@ -1,39 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 
-import reduxThunk from "redux-thunk";
-import reducer from './store/reducers';
-import firebase from './utils/firebase';
-
-const store = createStore(
-  reducer,
-  {},
-  applyMiddleware(reduxThunk)
-)
-
-const rrfProps = {
-  firebase,
-  config: {},
-  dispatch: store.dispatch,
-}
-
+import { store, rrfProps } from './store/storeConfig'
 
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
---
+++ react-redux-firebase-auth/src/store/storeConfig.js
@@ -0,0 +1,48 @@
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from "redux-thunk";
+import reducer from '../store/reducers';
+import firebase from '../utils/firebase';
+
+function saveToLocalStorage(state){
+    try{
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state',serializedState)
+    }catch (error){
+        console.log(error)
+    }
+}
+
+function loadFromLocalStorage(){
+    try{
+        const serializedState = localStorage.getItem('state')
+        if(serializedState === null) return undefined
+        return JSON.parse(serializedState)
+
+    }catch(error){
+        console.log(error)
+        return undefined
+    }
+}
+
+const persistedState = loadFromLocalStorage()
+
+const store = createStore(
+  reducer,
+  persistedState,
+  applyMiddleware(reduxThunk)
+)
+
+store.subscribe(
+    ()=>{
+        console.log('estado foi modificado!')
+        saveToLocalStorage(store.getState())
+    }
+)
+
+const rrfProps = {
+  firebase,
+  config: {},
+  dispatch: store.dispatch,
+}
+
+export { store, rrfProps}
